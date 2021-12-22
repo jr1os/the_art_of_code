a259660a699c6d1c391579954d0a6bf0165145e3
jr1os <dev.alirios@gmail.com>
2021-12-22 22:27:00 +0000

    Upgrade code :white_check_mark:

--- letter_counter.py
+++ letter_counter.py
@@ -1,12 +1,12 @@
 
-name = input("what is your name: ")
+name = input("what is your name: ").title().strip()
 print(f"Hello {name}")
 print("I will count the number of time that a specific letter occurs a message")
-message = input("Please enter a message: ")
+message = input("Please enter a message: ").lower()
 counter = 0
-letter_occurs = input("Wich letter would you like to count the occurences of: ")
+letter_occurs = input("Wich letter would you like to count the occurences of: ").lower()
 for letter in message:
     if letter ==  "h":
         counter += 1
     
-print(f"{name} your message has {counter} {letter}'s in it")
+print(f"{name} your message has {counter} {letter_occurs}'s in it")
