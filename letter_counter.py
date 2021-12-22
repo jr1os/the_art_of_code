5bd3074b1ccc8853396b9cde6f9fdc2d988f2efc
jr1os <dev.alirios@gmail.com>
2021-12-22 22:09:54 +0000

    upgrade code

--- letter_counter.py
+++ letter_counter.py
@@ -4,8 +4,9 @@ print(f"Hello {name}")
 print("I will count the number of time that a specific letter occurs a message")
 message = input("Please enter a message: ")
 counter = 0
+letter_occurs = input("Wich letter would you like to count the occurences of: ")
 for letter in message:
     if letter ==  "h":
         counter += 1
-print("Wich letter ")
     
+print(f"{name} your message has {counter} {letter}'s in it")
