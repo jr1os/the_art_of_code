144b562b2b0b35bb67cc039b667434a0a94c3f71
jr1os <dev.alirios@gmail.com>
2021-12-22 22:09:27 +0000

    Initial code of the project :tada:

---
+++ letter_counter.py
@@ -0,0 +1,11 @@
+
+name = input("what is your name: ")
+print(f"Hello {name}")
+print("I will count the number of time that a specific letter occurs a message")
+message = input("Please enter a message: ")
+counter = 0
+for letter in message:
+    if letter ==  "h":
+        counter += 1
+print("Wich letter ")
+    
