6314e4ec26fe0759f5138f5acffa553ae9467488
jr1os <dev.alirios@gmail.com>
2021-12-24 22:20:50 +0000

    added new code :zap:

---
+++ temp_conver.py
@@ -0,0 +1,7 @@
+print("Welcome to the temperature conversion program.")
+temp_f = float(input("what is the given temperature in degrees fahrenheit: "))
+temp_c = ((temp_f - 32) * 5) / 9
+temp_k = temp_c + 273.15
+print(f"Degrees Fahrenheit :\t {temp_f:.4f}")
+print(f"Degrees Celcius:\t {temp_c:.4f}")
+print(f"Degrees Kelvin:\t\t {temp_k:.4f}")
