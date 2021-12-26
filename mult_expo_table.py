a5a053ba67fe0a88d7f0ee56615982baba22446f
jr1os <dev.alirios@gmail.com>
2021-12-26 15:04:07 +0000

    added upgrade code Multiplication/exponent :zap:

--- mult_expo_table.py
+++ mult_expo_table.py
@@ -1,4 +1,21 @@
 print("Welcome to the Multiplication/Exponent Table App")
 
-name = input("Hello, What is your name: ")
+name = input("Hello, What is your name: ").strip()
 number = float(input("What number would you like to work with: "))
+
+print(f"Multiplication table for {number}")
+
+for n in range(10):
+    print(f"{n} * {number} = {n*number:.2f}")
+
+print(f"Exponent Table for {number}")
+
+for i in range(10):
+    print(f"{number} ** {i} = {number**i:.4f}")
+
+message = f"{name} Math is cool"
+
+print(message.title())
+print(f"\t {message.lower()}")
+print(f"\t\t {message.capitalize()}")
+
