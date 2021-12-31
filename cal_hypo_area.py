2a3830c41bf23475f3d2fa9e18f4f8706b0c16fa
jr1os <dev.alirios@gmail.com>
2021-12-31 19:07:09 +0000

    upgrade code.

--- cal_hypo_area.py
+++ cal_hypo_area.py
@@ -1,8 +1,9 @@
 import math
-print("Welcome to the right triangle solver app")
+print("Welcome to the right triangle solver App")
 
 side_a = float(input("What is the first leg of the triangle: "))
 side_b = float(input("What is the second leg of the triangle: "))
+
 
 hypotenuse = math.sqrt((side_a**2) + (side_b**2))
 area = (side_a * side_b) / 2
